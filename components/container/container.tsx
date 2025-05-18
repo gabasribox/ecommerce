64d2d0c1e6137913d67106382552744731b36c1c
gabasribox <gabasribox@outlook.com>
2025-05-18 10:50:33 -0300

    Container Update

--- components/container/container.tsx
+++ components/container/container.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import "./container.css";
 
+/* Container Types */
 type ContainerProps = {
   container_width: string;
   container_title: string;
@@ -8,12 +9,20 @@ type ContainerProps = {
 };
 
 const Container = ({ container_width, container_title, children }: ContainerProps) => {
+  /* The component */
   return (
+    /* The Container with your style and a variable to define externally your width when he is called */
     <div className={`container-styles ${container_width}`}>
+      
+      {/* Container Title */}
       <h1 className="container-title">
         {container_title}
       </h1>
-      {children}
+
+      {/* The content children of the container */}
+      <div className="container-children">
+        {children}
+      </div>
     </div>
   );
 }
